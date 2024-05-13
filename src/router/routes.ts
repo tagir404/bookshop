fd211321291aa4b5a07c0e703a4f42d92c42563c
Tagir <sulaev.tagir@mail.ru>
2024-05-13 23:36:09 +0300

    router upd

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import MainPage from '@/pages/MainPage.vue'
 
 const routes = [
@@ -7,7 +7,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes
 })
 
